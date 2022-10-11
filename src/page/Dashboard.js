ea462e864a608ff5b87f7aaf7f9b9e99e7e28689
Saksit Phoomkham <63160264@go.buu.ac.th>
2022-10-11 21:09:41 +0700

    Ex
    Dassbord-Ex

--- src/page/Dashboard.js
+++ src/page/Dashboard.js
@@ -1,10 +1,29 @@
 import { Link } from "react-router-dom";
+import './dass.css';
+import "../vendor/bootstrap-select/dist/css/bootstrap-select.min.css";
 
 const Dashboard = () => {
     return <div>
-    <h1 id="name">Dashboard</h1>
-    <button><Link to="/">Logout</Link></button>
+        <h1 id="name">Dashboard</h1>
+
+        <div className=".container-lg main">
+            <h1 className="header">ยินดีตอนรับเข้าสู่ Deep Dark Gyms</h1>
+            <div class="card-group">
+                <div class="card">
+                    <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/News/gym-membership/4489-gym_people_exercise-1296x728-header.jpg?w=1155&h=1528" class="card-img-top" alt="..." />
+                </div>
+                <div class="card">
+                    <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/News/gym-membership/4489-gym_people_exercise-1296x728-header.jpg?w=1155&h=1528" class="card-img-top" alt="..." />
+                </div>
+                <div class="card">
+                    <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/News/gym-membership/4489-gym_people_exercise-1296x728-header.jpg?w=1155&h=1528" class="card-img-top" alt="..." />
+                </div>
+              
+            </div>
+        </div>
     </div>;
+
+
 };
 
 export default Dashboard;
