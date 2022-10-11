53fdb206d7e6352b626d9253981a2b499b4be9f9
Saksit0 <63160264@go.buu.ac.th>
2022-10-12 00:05:57 +0700

    Ex
    update dashboard

--- src/page/Dashboard.js
+++ src/page/Dashboard.js
@@ -2,21 +2,22 @@ import { Link } from "react-router-dom";
 import './dass.css';
 import "../vendor/bootstrap-select/dist/css/bootstrap-select.min.css";
 
+
 const Dashboard = () => {
     return <div>
-        <h1 id="name">Dashboard</h1>
+        
 
-        <div className=".container-lg main">
+        <div className="container-lg   main">
             <h1 className="header">ยินดีตอนรับเข้าสู่ Deep Dark Gyms</h1>
-            <div class="card-group">
-                <div class="card">
+            <div class=" card-group  ">
+                <div >
+                    <img src="https://static.hdmall.co.th/1146x735/system/image_attachments/images/000/090/137/original/03.%E0%B8%84%E0%B8%AD%E0%B8%A3%E0%B9%8C%E0%B8%AA%E0%B8%AD%E0%B8%AD%E0%B8%81%E0%B8%81%E0%B8%B3%E0%B8%A5%E0%B8%B1%E0%B8%87%E0%B8%81%E0%B8%B2%E0%B8%A2%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B9%80%E0%B8%97%E0%B8%A3%E0%B8%99%E0%B9%80%E0%B8%99%E0%B8%AD%E0%B8%A3%E0%B9%8C%E0%B8%AA%E0%B9%88%E0%B8%A7%E0%B8%99%E0%B8%95%E0%B8%B1%E0%B8%A7_60_%E0%B8%99%E0%B8%B2%E0%B8%97%E0%B8%B5_20_%E0%B8%84%E0%B8%A3%E0%B8%B1%E0%B9%89%E0%B8%87_%E0%B8%9F%E0%B8%A3%E0%B8%B5!_3_%E0%B8%84%E0%B8%A3%E0%B8%B1%E0%B9%89%E0%B8%87___40_000.jpg" class="card-img-top" alt="..." />
+                </div>
+                <div >
                     <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/News/gym-membership/4489-gym_people_exercise-1296x728-header.jpg?w=1155&h=1528" class="card-img-top" alt="..." />
                 </div>
-                <div class="card">
-                    <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/News/gym-membership/4489-gym_people_exercise-1296x728-header.jpg?w=1155&h=1528" class="card-img-top" alt="..." />
-                </div>
-                <div class="card">
-                    <img src="https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/News/gym-membership/4489-gym_people_exercise-1296x728-header.jpg?w=1155&h=1528" class="card-img-top" alt="..." />
+                <div >
+                    <img src="http://www.healthaddict.com/uploads/ckeditor/images/H2%20series2.jpg" class="card-img-top" alt="..." />
                 </div>
               
             </div>
