a3a12cc56e17a24ae557e3a01b33767d1ae1e9ed
Saksit Phoomkham <63160264@go.buu.ac.th>
2022-10-13 16:17:27 +0700

    Ex
    update

--- src/page/member.js
+++ src/page/member.js
@@ -10,9 +10,12 @@ const member = () => {
               <div className="container-lg-12 mainpage">
                  <div className="container-lg-12 box-img">
                       <img src="https://www.travelgay.com/wp-content/uploads/2022/02/man-exercise-fitness-stockpack-pixabay-scaled.jpg" className="img1"></img>
-                       <div>
-                            <img></img>
-                       </div>
+                      <div className="img-menu">
+                            
+                            <img src="https://img.freepik.com/premium-photo/muscular-body-abs-six-pack-workout-sexy-muscular-fit-man-strong-torso-young-gay-man_265223-64099.jpg?w=2000" className="img2"></img>
+                            <img src="https://images.squarespace-cdn.com/content/v1/5d45a11f378ddd0001b7093e/1641923603274-2BF61JLN7BKYSRNY9NSY/unsplash-image-Th2Pk8NKpOc.jpg?format=2500w" className="img2"></img>
+                            <img src="https://images.squarespace-cdn.com/content/v1/5d45a11f378ddd0001b7093e/1641923603274-2BF61JLN7BKYSRNY9NSY/unsplash-image-Th2Pk8NKpOc.jpg?format=2500w" className="img2"></img>
+</div>
                  </div>
                      
              </div>
