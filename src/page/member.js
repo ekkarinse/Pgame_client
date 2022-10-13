565937bd04a10559b9463e3f49a0b20701df3446
Saksit Phoomkham <63160264@go.buu.ac.th>
2022-10-13 16:30:15 +0700

    ex
    update

--- src/page/member.js
+++ src/page/member.js
@@ -5,21 +5,21 @@ import "../vendor/bootstrap-select/dist/css/bootstrap-select.min.css";
 
 
 const member = () => {
-   
+
     return <div className="container-lg-12 con1">
-              <div className="container-lg-12 mainpage">
-                 <div className="container-lg-12 box-img">
-                      <img src="https://www.travelgay.com/wp-content/uploads/2022/02/man-exercise-fitness-stockpack-pixabay-scaled.jpg" className="img1"></img>
-                      <div className="img-menu">
-                            
-                            <img src="https://img.freepik.com/premium-photo/muscular-body-abs-six-pack-workout-sexy-muscular-fit-man-strong-torso-young-gay-man_265223-64099.jpg?w=2000" className="img2"></img>
-                            <img src="https://images.squarespace-cdn.com/content/v1/5d45a11f378ddd0001b7093e/1641923603274-2BF61JLN7BKYSRNY9NSY/unsplash-image-Th2Pk8NKpOc.jpg?format=2500w" className="img2"></img>
-                            <img src="https://images.squarespace-cdn.com/content/v1/5d45a11f378ddd0001b7093e/1641923603274-2BF61JLN7BKYSRNY9NSY/unsplash-image-Th2Pk8NKpOc.jpg?format=2500w" className="img2"></img>
-</div>
-                 </div>
-                     
-             </div>
-               
+        <div className="container-lg-12 mainpage">
+            <div className="container-lg-12 box-img">
+                <img src="https://www.travelgay.com/wp-content/uploads/2022/02/man-exercise-fitness-stockpack-pixabay-scaled.jpg" className="img1"></img>
+                <div className="img-menu">
+
+                    <img src="https://img.freepik.com/premium-photo/muscular-body-abs-six-pack-workout-sexy-muscular-fit-man-strong-torso-young-gay-man_265223-64099.jpg?w=2000" className="img2"></img>
+                    <img src="https://images.squarespace-cdn.com/content/v1/5d45a11f378ddd0001b7093e/1641923603274-2BF61JLN7BKYSRNY9NSY/unsplash-image-Th2Pk8NKpOc.jpg?format=2500w" className="img2"></img>
+                    <img src="https://images.squarespace-cdn.com/content/v1/5d45a11f378ddd0001b7093e/1641923603274-2BF61JLN7BKYSRNY9NSY/unsplash-image-Th2Pk8NKpOc.jpg?format=2500w" className="img2"></img>
+                </div>
+            </div>
+
+        </div>
+
     </div>;
 
 
