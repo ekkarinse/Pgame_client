85fc658f4455500cb4373e34750aca1802a422b6
Saksit Phoomkham <63160264@go.buu.ac.th>
2022-10-20 03:01:32 +0700

    xxx

--- src/page/Whey.js
+++ src/page/Whey.js
@@ -2,15 +2,53 @@ import { Link } from "react-router-dom";
 import './Whey.css';
 import "../vendor/bootstrap-select/dist/css/bootstrap-select.min.css";
 import pic from "../picture/whey.jpg";
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {Modal, Button} from 'react-bootstrap';
+import axios from "axios";
 
 const Whey = () => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const [inputWhey, setWhey] = useState({});
+    const getname = document.getElementById('name');
+
+    const getwhey = () => { axios.get('http://localhost:3004/product/select_whey').then(response => {
+        console.log(response.data);
+       
+            getname.innerHTML = response.data[0].name_whey;
+            return response.data[0].name_whey;
+     });
+ 
+     };
    
+     
+    // async function getwhey(){
+    //     const data = await axios.get("http://localhost:3004/product/select_whey").then(response => {
+    //         console.log(response.data[0]);
+    //         getname.innerHTML = response.data.name_whey;
+    //     });
+     
+        // axios({
+        //   method:"get",
+        //   url: "http://localhost:3004/product/select_whey",
+        //   header:{
+        //     "Content-Type": "application/json",
+        //   },
+        //   data:inputWhey,
+        // })
+        // .then(function(response){
+        //   console.log("success");
+        // //   console.log(response.data);
+        //   document.getElementById("wheyset").innerHTML = response.data;
+        // }).catch(function(error){
+        //   console.log("error");
+        // })
+    //   }
+
+    getwhey();
     return <div className="whey">
+        
         <div className="container">
             <h2><b>Whey Protein</b></h2>
             <br></br><br></br>
@@ -18,7 +56,9 @@ const Whey = () => {
                 <div>
                 <img src={pic} alt="pic" width="250" height="300"></img>
                 <br></br><br></br>
-                    <p>Optimum Nutrition Whey Protein Gold Standard 2LB</p>
+                {/* <button onClick={getwhey}>Test</button> */}
+                <h1>ชื่อ : <span id="name" ></span></h1>
+                    {/* <p>Optimum Nutrition Whey Protein Gold Standard 2LB</p> */}
                 {/* <br></br><br></br> */}
                 <Link to="" class="button" onClick={handleShow}><button className='button-header' class="btn btn-warning">1,250 Baht</button></Link> 
                 </div>
